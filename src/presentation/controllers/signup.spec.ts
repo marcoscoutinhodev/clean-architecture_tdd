9b7e8aba53c13f40a77bec62bf6541ddf1903a07
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-04 16:58:56 -0300

    test: ensure SignUpController returns 400 if birthdate is not provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -121,4 +121,24 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('rg'));
   });
+
+  test('Should return 400 if birthdate is not provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        cpf: 'any_cpf',
+        rg: 'any_rg',
+        cellphone: 'any_cellphone',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('birthdate'));
+  });
 });
