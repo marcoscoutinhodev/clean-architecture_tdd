8e6293ef6c31c72733439e0ad341c9156496be83
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-04 17:02:07 -0300

    test: ensure SignUpController returns 400 if cellphone is not provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -141,4 +141,24 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('birthdate'));
   });
+
+  test('Should return 400 if cellphone is not provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        cpf: 'any_cpf',
+        birthdate: 'any_brithdate',
+        rg: 'any_rg',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('cellphone'));
+  });
 });
