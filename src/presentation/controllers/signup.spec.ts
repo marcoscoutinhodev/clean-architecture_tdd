8caa8f54b307f671a6980b44bfcf6112f0cca5e0
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-05 11:39:33 -0300

    test: ensure SignUpController returns 500 if CellphoneValidator throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -516,4 +516,30 @@ describe('SignUp Controller', () => {
 
     expect(isValidSpy).toHaveBeenCalledWith('any_cellphone');
   });
+
+  test('Should return 500 if CellphoneValidator throws', () => {
+    const { sut, cellphoneValidatorStub } = makeSut();
+
+    jest.spyOn(cellphoneValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        cpf: 'any_cpf',
+        rg: 'any_rg',
+        birthdate: 'any_brithdate',
+        cellphone: 'any_cellphone',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
 });
