abfbf83519a32493d191dd305c44975bf847a842
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-05 14:33:43 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -489,7 +489,7 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password',
         cpf: 'any_cpf',
         rg: 'any_rg',
-        birthdate: 'any_brithdate',
+        birthdate: 'any_birthdate',
         cellphone: 'any_cellphone',
       },
     };
@@ -513,7 +513,7 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password',
         cpf: 'any_cpf',
         rg: 'any_rg',
-        birthdate: 'any_brithdate',
+        birthdate: 'any_birthdate',
         cellphone: 'any_cellphone',
       },
     };
@@ -537,7 +537,7 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password',
         cpf: 'any_cpf',
         rg: 'any_rg',
-        birthdate: 'any_brithdate',
+        birthdate: 'any_birthdate',
         cellphone: 'any_cellphone',
       },
     };
@@ -562,7 +562,7 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password',
         cpf: 'any_cpf',
         rg: 'any_rg',
-        birthdate: 'any_brithdate',
+        birthdate: 'any_birthdate',
         cellphone: 'any_cellphone',
       },
     };
@@ -586,7 +586,7 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password',
         cpf: 'any_cpf',
         rg: 'any_rg',
-        birthdate: 'any_brithdate',
+        birthdate: 'any_birthdate',
         cellphone: 'any_cellphone',
       },
     };
@@ -599,7 +599,7 @@ describe('SignUp Controller', () => {
       password: 'any_password',
       cpf: 'any_cpf',
       rg: 'any_rg',
-      birthdate: 'any_brithdate',
+      birthdate: 'any_birthdate',
       cellphone: 'any_cellphone',
     });
   });
@@ -619,7 +619,7 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password',
         cpf: 'any_cpf',
         rg: 'any_rg',
-        birthdate: 'any_brithdate',
+        birthdate: 'any_birthdate',
         cellphone: 'any_cellphone',
       },
     };
@@ -628,5 +628,35 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut, addAccountStub } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+        cpf: 'valid_cpf',
+        rg: 'valid_rg',
+        birthdate: 'valid_birthdate',
+        cellphone: 'valid_cellphone',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password',
+      cpf: 'valid_cpf',
+      rg: 'valid_rg',
+      birthdate: 'valid_birthdate',
+      cellphone: 'valid_cellphone',
+    });
   });
 });
