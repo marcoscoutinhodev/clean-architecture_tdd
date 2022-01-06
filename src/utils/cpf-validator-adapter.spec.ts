7f1efb2c281bde2bae7450aee16d1dfbedc158c2
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-06 15:12:25 -0300

    test: ensure CpfValidatorAdapter returns true if validator returns true

--- src/utils/cpf-validator-adapter.spec.ts
+++ src/utils/cpf-validator-adapter.spec.ts
@@ -1,11 +1,23 @@
 import { CpfValidatorAdapter } from './cpf-validator-adapter';
 
 describe('CpfValidator Adapter', () => {
+  const sut = new CpfValidatorAdapter();
+
+  beforeAll(() => {
+    jest.spyOn(sut, 'isValid').mockImplementation(() => true);
+  });
+
   test('Should return false if validator returns false', () => {
-    const sut = new CpfValidatorAdapter();
+    jest.spyOn(sut, 'isValid').mockReturnValueOnce(false);
 
     const isValid = sut.isValid('invalid_cpf');
 
     expect(isValid).toBe(false);
+  });
+
+  test('Should return true if validator returns true', () => {
+    const isValid = sut.isValid('valid_cpf');
+
+    expect(isValid).toBe(true);
   });
 });
