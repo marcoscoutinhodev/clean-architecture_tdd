e3c2d5bc57a3ff69fa1523f733bbf2f0b8397a46
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-06 15:11:46 -0300

    feat: ensure CpfValidatorAdapter returns true if validator returns true

--- src/utils/cpf-validator-adapter.ts
+++ src/utils/cpf-validator-adapter.ts
@@ -2,6 +2,50 @@ import { CpfValidator } from '../presentation/protocols/cpf-validator';
 
 export class CpfValidatorAdapter implements CpfValidator {
   isValid(cpf: string): boolean {
-    return false;
+    const formatedCpf: string = cpf.replace(/[^0-9]+/g, '');
+    // /^[0-9]{11}$/g
+    if (formatedCpf.length !== 11) {
+      return false;
+    }
+
+    let count: number = 10;
+    let result: number = 0;
+
+    for (let i = 0; i < 9; i++) {
+      result += parseInt(formatedCpf[i], 10) * count;
+
+      count--;
+    }
+
+    let remainder = (result * 10) % 11;
+
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (parseInt(formatedCpf[9], 10) !== remainder) {
+      return false;
+    }
+
+    result = 0;
+    count = 11;
+
+    for (let i = 0; i < 10; i++) {
+      result += parseInt(formatedCpf[i], 10) * count;
+
+      count--;
+    }
+
+    remainder = (result * 10) % 11;
+
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (parseInt(formatedCpf[10], 10) !== remainder) {
+      return false;
+    }
+
+    return true;
   }
 }
