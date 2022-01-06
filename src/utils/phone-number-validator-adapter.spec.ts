75ed594aef362df4f8a85c0cbcf8846faef137a0
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-06 13:05:37 -0300

    test: ensure PhoneNumberValidatorAdapter returns true if validator returns true

--- src/utils/phone-number-validator-adapter.spec.ts
+++ src/utils/phone-number-validator-adapter.spec.ts
@@ -1,11 +1,23 @@
 import { PhoneNumberValidatorAdapter } from './phone-number-validator-adapter';
 
 describe('PhoneNumberValidator Adapter', () => {
+  const sut = new PhoneNumberValidatorAdapter();
+
+  beforeEach(() => {
+    jest.spyOn(sut, 'isValid').mockImplementation(() => true);
+  });
+
   test('Should return false if validator returns false', () => {
-    const sut = new PhoneNumberValidatorAdapter();
+    jest.spyOn(sut, 'isValid').mockImplementationOnce(() => false);
 
     const isValid = sut.isValid('invalid_phone_number');
 
     expect(isValid).toBe(false);
+  });
+
+  test('Should return true if validator returns true', () => {
+    const isValid = sut.isValid('valid_phone_number');
+
+    expect(isValid).toBe(true);
   });
 });
