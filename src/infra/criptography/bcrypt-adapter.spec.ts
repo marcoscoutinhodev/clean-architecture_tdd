ebf2db4833732023639cb1e868941c7a11471110
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-07 11:40:05 -0300

    test: ensure BcryptAdapter returns a hash on success

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,28 @@
+import bcrypt from 'bcrypt';
+import { BcrypAdapter } from './bcrypt-adapter';
+
+jest.mock('bcrypt', () => ({
+  hash(): string {
+    return 'any_value';
+  },
+}));
+
+describe('Bcrypt Adapter', () => {
+  test('Should call bcrypt with correct values', async () => {
+    const salt = 12;
+    const sut = new BcrypAdapter(salt);
+    const hashSpy = jest.spyOn(bcrypt, 'hash');
+    await sut.encrypt('any_value');
+
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  test('Should return a hash on success', async () => {
+    const salt = 12;
+    const sut = new BcrypAdapter(salt);
+
+    const hash = await sut.encrypt('any_value');
+
+    expect(hash).toBe('any_value');
+  });
+});
