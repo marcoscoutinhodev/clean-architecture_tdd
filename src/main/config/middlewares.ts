d346596cd23b846d268135cb9eb20a2c43891e81
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-09 10:40:55 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlwares/body-parser';
+import { cors } from '../middlwares/cors';
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 };
