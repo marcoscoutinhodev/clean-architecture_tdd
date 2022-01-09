aac99d80bf72aa735ea8d7f6fc40958f5c3d4ed9
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-09 10:44:38 -0300

    fix: folder name

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,6 @@
 import { Express } from 'express';
-import { bodyParser } from '../middlwares/body-parser';
-import { cors } from '../middlwares/cors';
+import { bodyParser } from '../middlewares/body-parser';
+import { cors } from '../middlewares/cors';
 
 export default (app: Express): void => {
   app.use(bodyParser);
