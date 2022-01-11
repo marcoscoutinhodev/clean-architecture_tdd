857df7cd344f4ede9b665c95ef228b7c02aec120
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-11 19:37:38 -0300

    refactor: remove optional dependency

--- src/main/decorator/log.ts
+++ src/main/decorator/log.ts
@@ -4,7 +4,7 @@ import { LogErrorRepository } from '../../data/protocols/log-error-repository';
 export class LogControllerDecorator implements Controller {
   constructor(
     private readonly controller: Controller,
-    private readonly logErrorRepository?: LogErrorRepository,
+    private readonly logErrorRepository: LogErrorRepository,
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
