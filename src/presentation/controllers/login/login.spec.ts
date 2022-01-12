30f5c75709dce6d14334aaaa3b686b7d3204e921
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-12 09:53:19 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,7 +1,14 @@
 import { LoginController } from './login';
 import { badRequest } from '../../helpers/http-helper';
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { EmailValidator } from './login-protocols';
+import { EmailValidator, HttpRequest } from './login-protocols';
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@email.com',
+    password: 'any_password',
+  },
+});
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -33,7 +40,8 @@ describe('Login Controller', () => {
     const { sut } = makeSut();
     const httpRequest = {
       body: {
-        password: 'any_password',
+        ...makeFakeRequest().body,
+        email: undefined,
       },
     };
 
@@ -46,7 +54,8 @@ describe('Login Controller', () => {
     const { sut } = makeSut();
     const httpRequest = {
       body: {
-        email: 'any_email@email.com',
+        ...makeFakeRequest().body,
+        password: undefined,
       },
     };
 
@@ -57,15 +66,9 @@ describe('Login Controller', () => {
 
   test('Should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut();
+    const httpRequest = makeFakeRequest();
 
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
-
-    const httpRequest = {
-      body: {
-        email: 'any_email@email.com',
-        password: 'any_password',
-      },
-    };
 
     await sut.handle(httpRequest);
 
@@ -74,15 +77,9 @@ describe('Login Controller', () => {
 
   test('Should return 400 if an invalid email is provided', async () => {
     const { sut, emailValidatorStub } = makeSut();
+    const httpRequest = makeFakeRequest();
 
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
-
-    const httpRequest = {
-      body: {
-        email: 'invalid_email@email.com',
-        password: 'any_password',
-      },
-    };
 
     const httpResponse = await sut.handle(httpRequest);
 
