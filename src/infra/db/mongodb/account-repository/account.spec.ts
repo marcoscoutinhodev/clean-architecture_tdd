ff3fc11720862a547d05ac3174dfe509383f1f6a
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-22 17:50:12 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,7 +1,10 @@
+import { Collection } from 'mongodb';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { AccountMongoRepository } from './account';
 
 describe('Account MongoDB Repository', () => {
+  let accountCollection: Collection;
+
   beforeAll(async () => {
     await MongoHelper.connect(MongoHelper.MongoMemoryUriToTests);
   });
@@ -11,13 +14,13 @@ describe('Account MongoDB Repository', () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accouns');
+    accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
   });
 
   const makeSut = (): AccountMongoRepository => new AccountMongoRepository();
 
-  test('Should return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const sut = makeSut();
     const account = await sut.add({
       name: 'any_name',
@@ -38,5 +41,29 @@ describe('Account MongoDB Repository', () => {
     expect(account.rg).toBe('any_rg');
     expect(account.birthdate).toBe('any_birthdate');
     expect(account.phoneNumber).toBe('any_phone_number');
+  });
+
+  test('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut();
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password',
+      cpf: 'any_cpf',
+      rg: 'any_rg',
+      birthdate: 'any_birthdate',
+      phoneNumber: 'any_phone_number',
+    });
+    const account = await sut.loadByEmail('any_email@email.com');
+
+    expect(account).toBeTruthy();
+    expect(account!.id).toBeTruthy();
+    expect(account!.name).toBe('any_name');
+    expect(account!.email).toBe('any_email@email.com');
+    expect(account!.password).toBe('any_password');
+    expect(account!.cpf).toBe('any_cpf');
+    expect(account!.rg).toBe('any_rg');
+    expect(account!.birthdate).toBe('any_birthdate');
+    expect(account!.phoneNumber).toBe('any_phone_number');
   });
 });
