0624966c79feca4bb9ad9caf502d69973f2a9d09
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-01-22 17:54:54 -0300

    feat: ensure AccountMongoRepository returns null if loadByEmail fails

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -19,6 +19,6 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
 
     const account = await accountCollection.findOne({ email });
 
-    return MongoHelper.map(account);
+    return account ? MongoHelper.map(account) : null;
   }
 }
