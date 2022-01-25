ec43de4c9bb56ce3024768da3d994828f2a58032
marcoscoutinhodev <71992526+marcoscoutinhodev@users.noreply.github.com>
2022-01-25 17:49:35 -0300

    chore: delete dist directory

--- dist/utils/phone-number-validator-adapter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PhoneNumberValidatorAdapter = void 0;
-class PhoneNumberValidatorAdapter {
-    isValid(phoneNumber) {
-        return /^\d{2}-\d{5}-\d{4}$/g.test(phoneNumber);
-    }
-}
-exports.PhoneNumberValidatorAdapter = PhoneNumberValidatorAdapter;
