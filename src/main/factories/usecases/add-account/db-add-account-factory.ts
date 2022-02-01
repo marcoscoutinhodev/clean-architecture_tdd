3741e041fc1eadcf02da5caacc196b4809d42a91
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-01 11:45:28 -0300

    refactor: reusing variable

--- src/main/factories/usecases/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -7,7 +7,6 @@ export const makeDbAddAccount = (): AddAccount => {
   const salt = 12;
   const hasher = new BcrypAdapter(salt);
   const addAccountRepository = new AccountMongoRepository();
-  const loadAccountByEmailRepository = new AccountMongoRepository();
 
-  return new DbAddAccount(hasher, addAccountRepository, loadAccountByEmailRepository);
+  return new DbAddAccount(hasher, addAccountRepository, addAccountRepository);
 };
