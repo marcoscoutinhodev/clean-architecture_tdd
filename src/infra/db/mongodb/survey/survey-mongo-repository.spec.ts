e5a6943fbe82035f997a2f8a4a96dabcc78081ef
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-02 13:08:33 -0300

    test: ensure SurveyMongoRepository add a new survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,43 @@
+import { Collection } from 'mongodb';
+import { SurveyMongoRepository } from './survey-mongo-repository';
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey';
+import { MongoHelper } from '../helpers/mongo-helper';
+import { mongoUri } from '../../../../../globalConfig.json';
+
+const makeFakeAddSurveyModel = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer',
+  }, {
+    answer: 'other_answer',
+  }],
+});
+
+describe('Survey MongoDB Repository', () => {
+  let surveyCollection: Collection;
+
+  beforeAll(async () => {
+    await MongoHelper.connect(mongoUri);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany({});
+  });
+
+  const makeSut = (): SurveyMongoRepository => new SurveyMongoRepository();
+
+  test('Should add a survey success', async () => {
+    const sut = makeSut();
+    await sut.add(makeFakeAddSurveyModel());
+
+    const survey = await surveyCollection.findOne({ question: 'any_question' });
+
+    expect(survey).toBeTruthy();
+  });
+});
