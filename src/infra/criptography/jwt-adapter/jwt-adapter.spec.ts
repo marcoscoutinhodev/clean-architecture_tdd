efa5bde727516752f0c10734a33c9508b2f7a7ff
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-03 17:20:49 -0300

    test: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -5,6 +5,9 @@ jest.mock('jsonwebtoken', () => ({
   sign(): string {
     return 'any_token';
   },
+  verify(): string {
+    return 'any_value';
+  },
 }));
 
 const makeSut = (): JwtAdapter => new JwtAdapter('secret');
@@ -35,6 +38,17 @@ describe('Jwt Adapter', () => {
       const promise = sut.encrypt('any_value');
 
       expect(promise).rejects.toThrow();
+    });
+  });
+
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, 'verify');
+
+      await sut.decrypt('any_token');
+
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
     });
   });
 });
