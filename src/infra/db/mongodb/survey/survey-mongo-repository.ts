f6c0eab6e592ca94bebff5aae7cc0b1a338d0848
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-05 17:27:40 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,23 @@
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository';
+import { LoadSurveysRepository, SurveyModel } from '../../../../data/usecases/load-surveys/db-load-surveys-protocols';
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.insertOne(surveyData);
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    const surveys = await surveyCollection.find().toArray();
+    const mappedSurveys: SurveyModel[] = [];
+
+    surveys.forEach((survey) => {
+      mappedSurveys.push(MongoHelper.map(survey));
+    });
+
+    return mappedSurveys;
   }
 }
