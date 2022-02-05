a28a7c82a7438e0069b57e6c23645e938efa276d
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-05 17:33:27 -0300

    test: ensure SurveyMongoRepository returns an empty list if there are no surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -59,6 +59,16 @@ describe('Survey MongoDB Repository', () => {
       const surveys = await sut.loadAll();
 
       expect(surveys.length).toBe(2);
+      expect(surveys[0].question).toBe('any_question');
+      expect(surveys[1].question).toBe('other_question');
     });
+  });
+
+  test('Should return an empty list', async () => {
+    const sut = makeSut();
+
+    const surveys = await sut.loadAll();
+
+    expect(surveys.length).toBe(0);
   });
 });
