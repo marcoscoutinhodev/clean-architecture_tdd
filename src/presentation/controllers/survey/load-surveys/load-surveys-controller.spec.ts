a04c1b57287f9a571f20e45831d57afc461dcb29
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-05 13:52:44 -0300

    test: ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,4 +1,5 @@
-import { serverError } from '../../../helpers/http/http-helper';
+import MockDate from 'mockdate';
+import { ok, serverError } from '../../../helpers/http/http-helper';
 import { LoadSurveysController } from './load-surveys-controller';
 import { LoadSurveys, SurveyModel } from './load-surveys-controller-protocols';
 
@@ -52,6 +53,14 @@ const makeSut = (): SutTypes => {
 };
 
 describe('LoadSurveys Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
   test('Should call LoadSurveys', async () => {
     const { sut, loadSurveysStub } = makeSut();
     const loadSpy = jest.spyOn(loadSurveysStub, 'load');
@@ -59,6 +68,14 @@ describe('LoadSurveys Controller', () => {
     await sut.handle({});
 
     expect(loadSpy).toHaveBeenCalled();
+  });
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle({});
+
+    expect(httpResponse).toEqual(ok(makeFakeSurveys()));
   });
 
   test('Should return 500 if LoadSurveys throws', async () => {
