41a6eaa502d837e47dcd21bb3f87e80a408f0279
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-07 10:22:12 -0300

    test: ensure DbSaveSurveyResult returns a SurveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -58,6 +58,14 @@ describe('DbSaveSurveyResult Usecase', () => {
     expect(saveSpy).toHaveBeenCalledWith(surveyResultData);
   });
 
+  test('Should return SurveyResult on success', async () => {
+    const { sut } = makeSut();
+
+    const surveyResult = await sut.save(makeFakeSurveyResultData());
+
+    expect(surveyResult).toEqual(makeFakeSurveyResult());
+  });
+
   test('Should throw if SaveSurveyResultRepository throws', () => {
     const { sut, saveSurveyResultRepositoryStub } = makeSut();
     jest.spyOn(saveSurveyResultRepositoryStub, 'save').mockRejectedValueOnce(new Error());
