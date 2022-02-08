fe44d03e431529ba93ddf737b7e4ded6bd2a6b2b
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-08 11:48:10 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,5 +1,5 @@
 import {
-  Controller, HttpRequest, HttpResponse, LoadSurveyById,
+  Controller, HttpRequest, HttpResponse, LoadSurveyById, SaveSurveyResult,
 } from './save-survey-result-controller-protocols';
 import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper';
 import { InvalidParamError } from '@/presentation/errors';
@@ -7,6 +7,7 @@ import { InvalidParamError } from '@/presentation/errors';
 export class SaveSurveyResultController implements Controller {
   constructor(
     private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult,
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -24,6 +25,13 @@ export class SaveSurveyResultController implements Controller {
       if (!answers.includes(answer)) {
         return forbidden(new InvalidParamError('answer'));
       }
+
+      await this.saveSurveyResult.save({
+        surveyId,
+        accountId: httpRequest.accountId!,
+        answer,
+        date: new Date(),
+      });
 
       return ok({});
     } catch (error) {
