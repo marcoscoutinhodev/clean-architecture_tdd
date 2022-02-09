27b46468922040bf732c88853e64a2bc62285e4c
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-09 19:16:30 -0300

    refactor: covering all test lines

--- src/infra/validators/cpf-validator-adapter.spec.ts
+++ src/infra/validators/cpf-validator-adapter.spec.ts
@@ -3,29 +3,29 @@ import { CpfValidatorAdapter } from './cpf-validator-adapter';
 describe('CpfValidator Adapter', () => {
   const sut = new CpfValidatorAdapter();
 
-  beforeAll(() => {
-    jest.spyOn(sut, 'isValid').mockImplementation(() => true);
-  });
-
-  test('Should return false if validator returns false', () => {
-    jest.spyOn(sut, 'isValid').mockReturnValueOnce(false);
-
-    const isValid = sut.isValid('invalid_cpf');
-
-    expect(isValid).toBe(false);
-  });
-
-  test('Should return true if validator returns true', () => {
-    const isValid = sut.isValid('valid_cpf');
-
-    expect(isValid).toBe(true);
-  });
-
   test('Should call validator with correct cpf', () => {
     const isValidSpy = jest.spyOn(sut, 'isValid');
 
     sut.isValid('any_cpf');
 
     expect(isValidSpy).toHaveBeenCalledWith('any_cpf');
+  });
+
+  test('Should return false if cpf length is not 11', () => {
+    const isValid = sut.isValid('123');
+    expect(isValid).toBe(false);
+  });
+
+  test('Should return false if the calculations fails', () => {
+    let isValid = sut.isValid('000.000.000.10');
+    expect(isValid).toBe(false);
+
+    isValid = sut.isValid('000.000.000.01');
+    expect(isValid).toBe(false);
+  });
+
+  test('Should return true if the calculation succeeds', () => {
+    const isValid = sut.isValid('123.456.789.09');
+    expect(isValid).toBe(true);
   });
 });
