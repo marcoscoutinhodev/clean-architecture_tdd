3b690b448dd1c51938cd2ba0f735ba8c308266de
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-10 10:09:27 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'NodeJS, TypeScript, Clean Achirecture, Tdd, Docker, Ci/Cd, Swagger',
+    version: '1.0.0',
+  },
+};
