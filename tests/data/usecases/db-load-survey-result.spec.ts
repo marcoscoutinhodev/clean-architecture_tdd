a2012cd80c5b956c36bc2f4028d2b2d4a0fbfb3d
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-16 18:44:19 -0300

    test: ensure DbLoadSurveyResult returns a surveyResult on success

--- tests/data/usecases/db-load-survey-result.spec.ts
+++ tests/data/usecases/db-load-survey-result.spec.ts
@@ -1,6 +1,8 @@
+import MockDate from 'mockdate';
 import { DbLoadSurveyResult } from '@/data/usecases/survey-result/load-survey-result/db-load-survey-result';
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
 import { mockLoadSurveyResultRepository } from '@/tests/data/mocks';
+import { mockSurveyResultModel } from '@/tests/domain/mocks';
 
 type SutTypes = {
   sut: DbLoadSurveyResult
@@ -18,6 +20,14 @@ const makeSut = (): SutTypes => {
 };
 
 describe('DbLoadSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
   test('Should call LoadSurveyResultRepository with correct value', async () => {
     const { sut, loadSurveyResultRepositoryStub } = makeSut();
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId');
@@ -34,5 +44,13 @@ describe('DbLoadSurveyResult UseCase', () => {
     const promise = sut.load('any_survey_id');
 
     expect(promise).rejects.toThrow();
+  });
+
+  test('Should return an surveyResultModel on success ', async () => {
+    const { sut } = makeSut();
+
+    const surveyResult = await sut.load('any_survey_id');
+
+    expect(surveyResult).toEqual(mockSurveyResultModel());
   });
 });
