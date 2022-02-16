af150313058737d8d90852997ded6f8a329367ba
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-16 18:55:17 -0300

    feat: ensure SurveyResultMongoRepository implements load survey result repository

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -2,9 +2,12 @@ import { ObjectId } from 'mongodb';
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository';
 import { SurveyResultModel } from '@/domain/models/survey-result';
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result';
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
 import { MongoHelper, QueryBuilder } from '../helpers';
 
-export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+export class SurveyResultMongoRepository implements
+  SaveSurveyResultRepository,
+  LoadSurveyResultRepository {
   async save(data: SaveSurveyResultParams): Promise<SurveyResultModel> {
     const surveyResultCollection = await MongoHelper.getCollection('surveyResults');
     await surveyResultCollection.findOneAndUpdate({
@@ -20,12 +23,12 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
       returnDocument: 'after',
     });
 
-    const surveyResultMapped = await this.mapSurveyResult(data.surveyId);
+    const surveyResultMapped = await this.loadBySurveyId(data.surveyId);
 
     return surveyResultMapped;
   }
 
-  private async mapSurveyResult(surveyId: string): Promise<SurveyResultModel> {
+  async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
     const surveyResultCollection = await MongoHelper.getCollection('surveyResults');
     const query = new QueryBuilder()
       .match({
