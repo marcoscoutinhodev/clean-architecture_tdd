cb16c9f24479f67768a749e80fb82aedd20084a1
marcoscoutinhodev <marcoscoutinhodev@outlook.com>
2022-02-17 19:41:24 -0300

    docs: add load survey resuls

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -44,4 +44,40 @@ export const surveyResultPath = {
       },
     },
   },
+  get: {
+    security: [{
+      apiKeyAuth: [],
+    }],
+    tags: ['Survey'],
+    summary: 'API for list a survey answer',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string',
+      },
+    }],
+    responses: {
+      200: {
+        description: 'Successful',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/forbidden',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
 };
